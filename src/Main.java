8b763cc002271ce9b67210fa8a16f52ccc4aeef4
husnidin <matyaqubovhusnidin@mail.ru>
2022-05-30 21:06:12 +0500

    HelloOne

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        String name = "Xusnidin ";
+        String aa = " OK ";
+        Integer a =12;
+        Integer b = 25;
+        Integer c = a+b;
+
+        System.out.println(name + c + aa);
+    }
+
+}
